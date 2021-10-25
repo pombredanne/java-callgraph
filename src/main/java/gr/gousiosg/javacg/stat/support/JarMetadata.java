9fbea0c1bc205355694567e52ccd3eeaf39e3caf
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-10-25 12:34:38 -0500

    adding testMethods to JarMetadata

--- src/main/java/gr/gousiosg/javacg/stat/support/JarMetadata.java
+++ src/main/java/gr/gousiosg/javacg/stat/support/JarMetadata.java
@@ -17,6 +17,9 @@ public class JarMetadata implements Serializable {
   private transient final Reflections reflections;
   private transient final ClassHierarchyInspector inspector = new ClassHierarchyInspector();
 
+  /** Methods that are found in the jar for a program's tests */
+  private final Set<String> testMethods = new HashSet<>();
+
   /** Methods that are generated from the type erasure process */
   private final Set<String> bridgeMethods = new HashSet<>();
 
