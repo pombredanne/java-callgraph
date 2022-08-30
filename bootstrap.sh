bf73cb3bab8b4ba82f4a4c36e5c7accc958a24ce
Kinstabyou <rahman.maazur@gmail.com>
2022-08-30 15:35:38 -0500

    Changed shell file to accomodate jcg repo copying from vagrantfile directory instead of cloning from github repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,9 @@ echo 'maven install'
 apt-get install graphviz -y
 echo 'graphviz install'
 cd ~/Desktop
-git clone https://github.com/bitslab/java-callgraph
+# git clone https://github.com/bitslab/java-callgraph
+# /vagrant not mounting on my system by default currently. It is classified as an abnormal behavior and should work in general scenarios.
+# added logic for package building after copying from vagrant file directory.
+cp /vagrant ~/Desktop/java-callgraph
 cd java-callgraph
 mvn install
