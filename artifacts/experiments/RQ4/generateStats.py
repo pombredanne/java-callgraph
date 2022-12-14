8efd29028a19f0a1590c5886fff547775164d03a
Alekh Meka <60158358+ameka4@users.noreply.github.com>
2022-12-13 18:12:29 -0600

    Added Naive List Configuration Files (#30)
    * Added updated patch file w/ Naive List gen

--- artifacts/experiments/RQ4/generateStats.py
+++ artifacts/experiments/RQ4/generateStats.py
@@ -14,6 +14,12 @@ def test_fixed_properties(project_name: str):
     subprocess.run(["java", "-jar", JAR_FILE, "test", "-c",
                      project_fixed_name, "-f", project_graph])
 
+def test_naive_properties(project_name: str):
+    project_naive_name = project_name + "-naive"
+    project_graph = project_naive_name + "_graph" if project_name != "convex" else project_name + "-core-fixed_graph"
+    subprocess.run(["java", "-jar", JAR_FILE, "test", "-c",
+                    project_naive_name, "-f", project_graph])
+
 
 def main():
     if not sys.argv[1]:
@@ -23,6 +29,9 @@ def main():
         test_properties(project_name)
     for index in range(10):
         test_fixed_properties(project_name)
+    if project_name == "mph-table":
+        for index in range(10):
+            test_naive_properties(project_name)
 
 if __name__ == "__main__":
     main()
