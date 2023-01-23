ff36b445cd6b056873ec3d9c0e16215f83d0d6d9
Alekh Meka <60158358+ameka4@users.noreply.github.com>
2023-01-23 18:59:28 -0500

    Updated RQ2 shell scripts to use '-c' build flag (#35)
    * Added updated patch file w/ Naive List gen
    
    * Cleanup unused patch file
    
    * Updated generateResults to use naive list
    
    * Updated RQ4 with list naive
    
    * Updated project name to latex constant in table
    
    * Fresh results - rq4
    
    * Updated project name to latex constant in table
    
    * RQ4 Naive List (#32)
    
    * Added updated patch file w/ Naive List gen
    
    * Added mph-table-naive for vanilla implementation w/ increase in list size
    
    * Updated generateStats with List Naive
    
    * Patch/yaml changes for naive list
    
    * Cleanup unused patch file
    
    * Updated generateResults to use naive list
    
    * Updated RQ4 with list naive
    
    * Updated project name to latex constant in table
    
    * Updated project name to latex constant in table
    
    * Updated project name to latex constant in table
    
    * Cleanup print statements
    
    * Updated RQ2
    
    * Updated RQ2
    
    * Updated RQ2
    
    * Updated RQ2
    
    * Resolved RQ2 issue with mph-table
    
    * Resolved RQ2 issue with mph-table
    
    * Resolved RQ2 issue with mph-table
    
    Co-authored-by: Alekh Meka <Alekh.Meka@cat.com>

--- artifacts/experiments/RQ2/build_mph.sh
+++ artifacts/experiments/RQ2/build_mph.sh
@@ -4,8 +4,8 @@ java -jar target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar git -c mph-table-
 java -jar target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar git -c mph-table-500
 java -jar target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar git -c mph-table-1000
 
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table/mph-table-1.0.6-SNAPSHOT.jar -o mph-table_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-10/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-10/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-10_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-50/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-50/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-50_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-500/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-500/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-500_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-1000/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-1000/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-1000_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table -o mph-table_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-10 -o mph-table-10_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-50 -o mph-table-50_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-500 -o mph-table-500_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-1000 -o mph-table-1000_graph
--- artifacts/experiments/RQ2/build_trials.sh
+++ artifacts/experiments/RQ2/build_trials.sh
@@ -15,22 +15,22 @@ java -jar target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar git -c rpki-commo
 java -jar target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar git -c rpki-commons-500
 java -jar target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar git -c rpki-commons-1000
 
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-10/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-10/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-10_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-50/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-50/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-50_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-500/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-500/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-500_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-1000/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-1000/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-1000_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-10 -o mph-table-10_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-50 -o mph-table-50_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-500 -o mph-table-500_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-1000 -o mph-table-1000_graph
 
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/convex-10/convex-core/convex-core-0.7.1-jar-with-dependencies.jar -t ./artifacts/output/convex-10/convex-core/convex-core-0.7.1-tests.jar -o convex-10_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/convex-50/convex-core/convex-core-0.7.1-jar-with-dependencies.jar -t ./artifacts/output/convex-50/convex-core/convex-core-0.7.1-tests.jar -o convex-50_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/convex-500/convex-core/convex-core-0.7.1-jar-with-dependencies.jar -t ./artifacts/output/convex-500/convex-core/convex-core-0.7.1-tests.jar -o convex-500_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/convex-1000/convex-core/convex-core-0.7.1-jar-with-dependencies.jar -t ./artifacts/output/convex-1000/convex-core/convex-core-0.7.1-tests.jar -o convex-1000_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c convex-10 -o convex-10_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c convex-50 -o convex-50_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c convex-500 -o convex-500_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c convex-1000 -o convex-1000_graph
 
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/jflex-10/jflex/jflex-1.8.2-jar-with-dependencies.jar -t ./artifacts/output/jflex-10/jflex/jflex-1.8.2-tests.jar -o jflex-10_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/jflex-50/jflex/jflex-1.8.2-jar-with-dependencies.jar -t ./artifacts/output/jflex-50/jflex/jflex-1.8.2-tests.jar -o jflex-50_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/jflex-500/jflex/jflex-1.8.2-jar-with-dependencies.jar -t ./artifacts/output/jflex-500/jflex/jflex-1.8.2-tests.jar -o jflex-500_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/jflex-1000/jflex/jflex-1.8.2-jar-with-dependencies.jar -t ./artifacts/output/jflex-1000/jflex/jflex-1.8.2-tests.jar -o jflex-1000_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c jflex-10 -o jflex-10_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c jflex-50 -o jflex-50_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c jflex-500 -o jflex-500_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c jflex-1000 -o jflex-1000_graph
 
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/rpki-commons-10/rpki-commons-DEV.jar -t ./artifacts/output/rpki-commons-10/rpki-commons-DEV-tests.jar -o rpki-commons-10_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/rpki-commons-50/rpki-commons-DEV.jar -t ./artifacts/output/rpki-commons-50/rpki-commons-DEV-tests.jar -o rpki-commons-50_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/rpki-commons-500/rpki-commons-DEV.jar -t ./artifacts/output/rpki-commons-500/rpki-commons-DEV-tests.jar -o rpki-commons-500_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/rpki-commons-1000/rpki-commons-DEV.jar -t ./artifacts/output/rpki-commons-1000/rpki-commons-DEV-tests.jar -o rpki-commons-1000_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c rpki-commons-10 -o rpki-commons-10_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c rpki-commons-50 -o rpki-commons-50_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c rpki-commons-500 -o rpki-commons-500_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c rpki-commons-1000 -o rpki-commons-1000_graph
